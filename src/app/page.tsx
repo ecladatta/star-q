610ffc485380fe21fb8b1967c5a17b62e32afeb7
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2026-01-26 11:08:56 +0100

    fix: force dynamic rendering of Corpuses page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getCorpuses } from '@/actions/corpus/corpusActions'
 import { Corpuses } from '@/components/corpus'
 
+export const dynamic = 'force-dynamic'
+
 export default async function CorpusesPage() {
   const corpuses = await getCorpuses()
   return <Corpuses corpuses={corpuses} />
