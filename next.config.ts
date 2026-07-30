3feb05bc906e79f2e0118d8b3d8273be39d3b422
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2026-07-30 13:04:48 +0200

    feat: increase max body size for proxy client
    - Set `proxyClientMaxBodySize` to 100mb
    - Set `bodySizeLimit` for server actions to 100mb

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
+    proxyClientMaxBodySize: '100mb',
     serverActions: {
       bodySizeLimit: '100mb',
     },
